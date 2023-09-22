2929601d0b6bd22b8a3df0677653f7925f75f133
André <andregparada@gmail.com>
2023-09-22 10:12:17 -0300

    criado buttontext

---
+++ src/components/ButtonText/index.jsx
@@ -0,0 +1,13 @@
+import { Container} from "./styles";
+
+export function ButtonText({ title, icon }, ...rest) {
+    return(
+        <Container 
+            {...rest}
+            type="button"
+        >
+            {icon}
+            {title}
+        </Container>
+    );
+} 
